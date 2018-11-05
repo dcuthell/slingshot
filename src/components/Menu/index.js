3d6f6fa77094643f8cc98d873995e8c4f08c7bf2
David Cuthell <dcc1447@gmail.com>
2018-11-05 08:21:59 -0800

    add media queries for text size

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -71,35 +71,6 @@ render() {
           <p>cava brandy, blood orange puree</p>
         </Col>
       </Row>
-      <Row>
-        <Col>
-          <h1> Secondary Font Options </h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1 style={{fontFamily: 'Open Sans'}}>Open Sans</h1>
-          <p style={{fontFamily: 'Open Sans'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1 style={{fontFamily: 'Raleway'}}>Raleway</h1>
-          <p style={{fontFamily: 'Raleway'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1 style={{fontFamily: 'Abril Fatface'}}>Abril Fatface</h1>
-          <p style={{fontFamily: 'Abril Fatface'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1 style={{fontFamily: 'Roboto'}}>Roboto</h1>
-          <p style={{fontFamily: 'Roboto'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </Col>
-      </Row>
     </Container>
     );
   }
