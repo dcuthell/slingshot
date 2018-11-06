6e30340575ffbbde98a14000c7d925ec2e7e613a
David Cuthell <dcc1447@gmail.com>
2018-11-06 13:19:07 -0800

    turn home into pure component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,40 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Col, Row } from 'reactstrap';
-import './styles.css';
 
 import FBEmbedFrame from '../FBEmbedFrame';
 import InstagramFrame from '../InstagramFrame';
 
-
-class Home extends Component {
-  render() {
-    return (
-      <Container className="Home">
-        <Row>
-          <Col>
-            <h1>Welcome to the Slingshot Lounge</h1>
-            <h3>Happy Hour 3-7pm Mon-Fri</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h4>Instagram Activity</h4>
-          </Col>
-        </Row>
-        <InstagramFrame />
-        <Row>
-          <Col>
-            <h4>Recent Facebook Posts</h4>
-            <FBEmbedFrame data="timeline" account="slingshotlounge"/>
-          </Col>
-          <Col>
-            <h4>Events</h4>
-            <FBEmbedFrame data="events" account="slingshotlounge"/>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
-
-export default Home;
+export default function Home (props) {
+  return (
+    <Container className="Home">
+      <Row>
+        <Col>
+          <h1>Welcome to the Slingshot Lounge</h1>
+          <h3>Happy Hour 3-7pm Mon-Fri</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h4>Instagram Activity</h4>
+        </Col>
+      </Row>
+      <InstagramFrame />
+      <Row>
+        <Col>
+          <h4>Recent Facebook Posts</h4>
+          <FBEmbedFrame data="timeline" account="slingshotlounge"/>
+        </Col>
+        <Col>
+          <h4>Events</h4>
+          <FBEmbedFrame data="events" account="slingshotlounge"/>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
