671acbe0fab6c3c18028845db0f2412b3be50f1e
David Cuthell <dcc1447@gmail.com>
2018-11-11 17:33:21 -0500

    update api info for IG

--- src/components/InstagramFrame/index.js
+++ src/components/InstagramFrame/index.js
@@ -22,6 +22,7 @@ class InstagramFrame extends Component {
     .then(
       (result) => {
 				let myMap = result.data;
+				console.log(result);
 				myMap.forEach( (data) => {
 					this.setState({
 						urls: [
