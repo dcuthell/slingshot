4479696d9fb68e6cf29c03f27e894c99da0596f2
David Cuthell <dcc1447@gmail.com>
2018-11-11 17:22:24 -0500

    replace user-patched ig embed with updated version

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 ReactDOM.render(
-  <HashRouter basename={ process.env.PUBLIC_URL }>
+  <BrowserRouter basename={ process.env.PUBLIC_URL }>
     <App />
-  </HashRouter>, document.getElementById('root'));
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
